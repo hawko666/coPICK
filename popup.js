6a8e373766255b72e1e5cd385407917c44cc5f6a
Manthan Sawant <Manthan.Sawant@teknopoint.local>
2023-05-24 16:16:52 +0530

    changes

--- popup.js
+++ popup.js
@@ -4,17 +4,18 @@ const colorValue = document.querySelector(".color_value");
 const bodyColor = document.querySelector("body");
 const colorGridRgb = document.querySelector(".color_value_RGB")
 const copyBtn =  document.querySelector(".Rgb_copy_btn")
+let currentColor = "";
  
 btn.addEventListener('click', async () => {
-    const color = chrome.storage.sync.get("color", () => {
-        console.log('color', color);
+    const colorBg = chrome.storage.sync.get("color", () => {
+        console.log('color', colorBg);
     })
     let [tab] = await chrome.tabs.query({active:true, currentWindow: true});
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: pickColor,
     }, async(injectionResult) => {
-        const [data] = injectionResult;
+        let [data] = injectionResult;
         if(data.result){
             const color = data.result.sRGBHex;
             const colorRgb = hexToRGB(color);
@@ -22,8 +23,29 @@ btn.addEventListener('click', async () => {
             colorValue.innerText = color;
             bodyColor.style.backgroundColor = color;
             colorGridRgb.innerText = colorRgb;
+            currentColor = color;
             try{
                 await navigator.clipboard.writeText(color);
+            } catch (err){
+                console.error(err);
+            }
+            
+        }
+    });
+});
+
+copyBtn.addEventListener('click', async () =>{
+    let [tab] = await chrome.tabs.query({active:true, currentWindow: true});
+    console.log(tab)
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: copyFun,
+    }, async(injectionResult) => {
+        if(currentColor){
+            const color = currentColor;
+            const colorRgb = hexToRGB(color);
+            try{
+                await navigator.clipboard.writeText(colorRgb);
             } catch (err){
                 console.error(err);
             }
@@ -44,13 +66,14 @@ async function pickColor() {
     }
 }
 
-copyBtn.addEventListener('click', async () =>{
+async function copyFun() {
     try{
-        await navigator.clipboard.writeText(color);
-    } catch (err){
+        return currentColor;
+
+    } catch (err) {
         console.error(err);
     }
-});
+}
 
 function hexToRGB(hex) {
     const r = parseInt(hex.slice(1, 3), 16);
